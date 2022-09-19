249df13d2ae895ef0a9d03d3911b23a46dba5628
Subi-Babu <subiaby4613@gmail.com>
2022-09-19 15:39:20 +0530

    ok

---
+++ bin/sample.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main( ) {
+  String name = 'Aadhi';
+  int age = 20;
+  double mark=57;
+  String email = "fzfxc@gmail.com";
+  int phone=4567893212;
+
+
+  print("my name is $name");
+  print("i am $age years old");
+  print("mark percentage is $mark");
+  stdout.write("my mail id is $email");
+  print("my phone number $phone");
+} 
