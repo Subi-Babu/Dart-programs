0b03b029cf724d7b05d1b628f1de2acc30694ccd
Subi-Babu <subiaby4613@gmail.com>
2022-09-21 15:06:57 +0530

    commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  String name = "subi";
+  name = "anu";
+  //name = 20;
+
+  var name1 = "hello";
+  name1 = "hai";
+  //name1 = 20;
+
+  dynamic name2 = "good";
+  name2 = "morning";
+  name2 = 400;
+   
+
+   final dob = "20/7/1999";
+}
