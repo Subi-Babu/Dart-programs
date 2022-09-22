ffa0e833f25af8ec959dfc147b3011123eec43f2
Subi-Babu <subiaby4613@gmail.com>
2022-09-22 14:44:04 +0530

    commit

---
+++ bin/operators.dart
@@ -0,0 +1,12 @@
+void main() {
+ int x = 10, y = 5;
+print("arithmetic operations");
+print("x + y = ${x+y}");
+print("x - y = ${x-y}");
+print("x * y = ${x*y}");
+print("x / y = ${x/y}");
+print("x ~/ y = ${x~/y}");
+print("x % y = ${x%y}");
+print("-(x - y) = ${-(x-y)}");
+ 
+}
