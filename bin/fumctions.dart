0ece2bef7e3042c34a40586e658f154223818802
Subi-Babu <subiaby4613@gmail.com>
2022-09-22 14:12:36 +0530

    commit

--- bin/fumctions.dart
+++ bin/fumctions.dart
@@ -1,30 +1,22 @@
-void sum(){
-  int a = 30;
-  int b = 40;
-  print("sum = ${a+b}");
+void sum(int a, int b){
+    print("sum = ${a+b}");
 }
 
-void sub(){
-  int a = 30;
-  int b = 5;
-  print("subtraction = ${a-b}");
+void sub(int a,int b){
+    print("subtraction = ${a-b}");
 }
 
-void div(){
-  int a = 60;
-  int b = 6;
-  print("division = ${a/b}");
+void div(int a,int b){
+    print("division = ${a/b}");
 }
 
-void mul(){
-  int a = 30;
-  int b = 40;
+void mul(int a,int b){
   print("multiplication = ${a*b}");
 }
 
 void main(){
-  sum();
-  sub();
-  div();
-  mul();
+ sum(40,20);
+  sub(500,250);
+  div(45,9);
+  mul(20,10);
 }
---
+++ bin/function1.dart
@@ -0,0 +1,13 @@
+void add(int a, int b){ // here a and b are parameters /arguments / formal parameters
+  print('sum = ${a+b}');
+}
+void showmydetails(String name, int age , String email, String Qualification, int phone){
+
+}
+
+void main(){
+  add(43,56);
+  add(40, 20);
+  showmydetails('name',20, 'sdsff', 'gfgh', 9876564534231);
+}
+
