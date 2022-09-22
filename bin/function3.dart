977f74db6f5486ba145701a3e550639fd6b33a5e
Subi-Babu <subiaby4613@gmail.com>
2022-09-22 14:31:01 +0530

    commit

---
+++ bin/function3.dart
@@ -0,0 +1,22 @@
+void sum(int a ,int b){
+  print("sum = ${a+b}");
+}
+
+void sub(int a,int b){
+   print("subtraction = ${a-b}");
+}
+
+void div(int a,int b){
+    print("division = ${a/b}");
+}
+
+void mul(int a, int b){  
+  print("multiplication = ${a*b}");
+}
+
+void main(){
+  sum(12,10);
+  sub(400,100);
+  div(45,9);
+  mul(10,5);
+}
