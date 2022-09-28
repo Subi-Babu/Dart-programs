334031e215cae005aa22a30e7aec8966ed93d71a
Subi-Babu <108115666+Subi-Babu@users.noreply.github.com>
2022-09-28 13:18:16 +0530

    commit

--- bin/Assignment1/multiplication.dart
+++ bin/Assignment1/multiplication.dart
@@ -3,7 +3,8 @@ void main() {
   int i;
   print("Enter number to print table:");
   int ? num = int.parse(stdin.readLineSync()!);
-  for(i =1; i<=10; ++i){
+  for(i =1; i<=10; ++i)
+  {
  print("$num * $i = ${num * i} ");
   }
 }
