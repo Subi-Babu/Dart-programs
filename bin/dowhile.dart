a4d1d337993f6f292b2a32eec191c12392606572
Subi-Babu <subiaby4613@gmail.com>
2022-09-28 14:59:22 +0530

    commit

---
+++ bin/dowhile.dart
@@ -0,0 +1,10 @@
+void main()
+{
+    int j = 10;
+
+     do{
+       print(j);
+       j--;
+      }
+      while(j >= 1);
+}
---
+++ bin/whileloop.dart
@@ -0,0 +1,15 @@
+void main()
+{
+  int i = 1,sum = 0;
+ /* while(i <= 10)
+  {
+    print(i);
+    i++;
+  }*/
+  while(i<=10)
+    {
+    sum = sum+i;
+    i++;
+   }
+   print(sum);
+}
