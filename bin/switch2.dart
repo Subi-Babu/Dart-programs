751089e20f7a6dd2d6e81102aa7b0e5d4e57f8e0
Subi-Babu <subiaby4613@gmail.com>
2022-09-28 12:10:08 +0530

    commit

---
+++ bin/switch2.dart
@@ -0,0 +1,17 @@
+void main(){
+   var month = "september";
+   switch(month){
+     case "january" : print('month is january');
+     break;
+     case "february" : print('month is february');
+     break;
+     case "april" : print('month is april');
+     break;
+     case 'march' : print('month is march');
+     break;
+     case 'november' : print('month is november');
+     break;
+     default : print('none of these');
+
+   }
+}
