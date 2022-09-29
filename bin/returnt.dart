b675a761ac08386e6fb55fbc62d6b3eb22c771ef
Subi-Babu <subiaby4613@gmail.com>
2022-09-29 14:57:38 +0530

    commit

---
+++ bin/returnt.dart
@@ -0,0 +1,10 @@
+int add(){//default function with return type
+  int sum = 20 + 70;
+  return sum;
+  }
+  void main(){
+     //print(add());// frst option
+     int s = add();
+     print(s);
+
+  }
