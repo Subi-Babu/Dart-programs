22d50b4c1cf1d1626df96ea421332af76303be62
Subi-Babu <subiaby4613@gmail.com>
2022-10-06 20:20:12 +0530

    commit

--- bin/assignment3.dart
+++ bin/assignment3.dart
@@ -1,16 +1,28 @@
 import 'dart:io';
-void func(String name, String housename, String email,int age,int phn,{ required int pin, required double dob}){
-		
-	}
-
-
+void func(String name, String housename,int age,int phn,{ required int pin, required int dob}){
+	     
+       print("Enter your name");
+      var name1 = stdin.readLineSync();
+       print("Enter your housename");
+      var housename1 = stdin.readLineSync();
+        print("Enter your age");
+       int ? age1 = int.parse(stdin.readLineSync()!);
+        print("Enter your phoneno");
+      int ? ph1 = int.parse(stdin.readLineSync()!);
+       print("Enter your pincode");
+       int ? pin1 = int.parse(stdin.readLineSync()!);
+       print("Enter your dob");
+       int dob = int.parse(stdin.readLineSync()!);
+       print("name = $name1");
+        print("housename = $housename1");
+        print("age = $age1");
+        print("phonenumber = $ph1");
+        print("pin code = $pin1");
+        print("date of birth = $dob");
+  
+ 
+}
 	void main()
 	{
-
-		
-		scholar("Ankita", "Saini");
-		scholar("Siya", "Joshi", 30);
-		scholar("Rohan", "Joshi", 37,
-		"Information Technology");
-	}
-
+     func("samm", "sdfghjk", 28, 94561255, pin:456, dob: 2001-07-06);
+   	  }
