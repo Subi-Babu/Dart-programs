08d4ac091eb532efe9f86811e0948c6e34386b3d
Subi-Babu <subiaby4613@gmail.com>
2022-10-06 11:48:58 +0530

    commit

---
+++ bin/Assignment2/sum_of_ele.dart
@@ -0,0 +1,9 @@
+
+void main(){
+List <int> list2 = [1,-3,7,9,-6,0,-2,4,0,10,-8,5];
+  int sum =0;
+   for(int i = 0 ; i < list2.length ;i++){
+     sum += list2[i];
+   }
+   print('sum of numbers = $sum');
+  }
