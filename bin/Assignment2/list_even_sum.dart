ffbe09f69b4796dc0442ba47e83a57b197445264
Subi-Babu <subiaby4613@gmail.com>
2022-10-06 11:46:41 +0530

    commit

--- bin/Assignment2/list_even_sum.dart
+++ bin/Assignment2/list_even_sum.dart
@@ -1,4 +1,12 @@
-void main(){
-List <int> list2 = [1,-3,7,9,0,-6,-2,4,0,10,-8,5];
+void main(List<String> args){
+  int sum = 0;
 
+List<int> lists = [4,8,2,10];
+
+  for (int i =0; i < lists.length; i+=2){
+
+    sum += lists[i];
   }
+  print("Sum :  ${sum}");
+
+}
