ef9f910f077ecb326586336dd6eef55d1f278e99
Subi-Babu <subiaby4613@gmail.com>
2022-10-07 13:19:38 +0530

    commit

--- bin/Assignment2/list_even_sum.dart
+++ bin/Assignment2/list_even_sum.dart
@@ -1,12 +1,10 @@
 //find the sum of even number from the above list
 void main(List<String> args){
-  int sum = 0;
-List<int> lists = [1,-3,7,9,-6,0,-2,4,0,10,-8,5];
-
-  for (int i =2; i < lists.length; i+=2){
-
-    sum += lists[i];
-  }
-  print("Sum :  ${sum}");
+int sum = 0;
+List  lists = [1,-3,7,9,-6,0,-2,4,0,10,-8,5];
+ for (int i =2; i < lists.length; i+=2){   
+       sum =  lists[i];
+ }
+print("Sum :  $sum");
 
 }
--- bin/Assignment2/multipleoftwo.dart
+++ bin/Assignment2/multipleoftwo.dart
@@ -1,4 +1,6 @@
 // find the numbers which are multiple of 2 from the above list
 void main(){
 List  list2 = [1,-3,7,9,-6,0,-2,4,0,10,-8,5];
+var p = list2.where((x) => x % 2 == 0);
+print("multiples of 2 = $p");
 }
